3d5e229b60171f1909d6bba8f69b229e909c3354
junseok2516 <junseok2516@gmail.com>
2024-01-06 20:31:29 -0500

    240106*

---
+++ src/ValidParentheses20.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+public class ValidParentheses20 {
+    /* not able to think of using stack properly */
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (stack.isEmpty() || stack.pop() != c) {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
